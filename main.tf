05d7097467574d10e530168348df9cc5e5c3df5f
Khaleel <khaleelshaik711@gmail.com>
2025-10-29 11:03:34 +0530

    terraform with god pipelines

---
+++ main.tf
@@ -0,0 +1,60 @@
+resource "aws_security_group" "example" {
+  name        = "${var.name}-${env}"
+  description = "${var.name}-${env}"
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port        = var.port_no
+    to_port          = var.port_no
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+
+resource "aws_instance" "node" {
+  ami           = data.aws.ami.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.main.id]
+
+  tags = {
+    Name = "${var.name}-${env}"
+  }
+}
+# Create DNS Record in Route53
+resource "aws_route53_record" "record" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "${var.name}-${env}.khaleel221.shop"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.node.private_ip]
+
+  depends_on = [aws_instance.node] # ensures EC2 is created first
+}
+resource "null_resource" "provisioner" {
+  depends_on = [aws_route53_record.record]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      sleep 120;
+      cd /home/ec2-user/Expense-Ansible/;
+      ansible-playbook -i ${aws_instance.node.private_ip}, \
+        -e ansible_user=ec2-user \
+        -e ansible_password=DevOps321 \
+        -e role_name=${var.name} \
+        -e env=${var.env}\
+        expense.yml
+    EOT
+  }
+}
